39df1799d97f905599be86832807a7e4053965c4
George Yord <george.yord@gmail.com>
2021-03-23 17:09:04 +0200

    Enhance docker image to include: - gcc - go lang - aws cli

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "${SCRIPT_DIR}" || exit
+
 docker build . \
   --tag gcloud-tf:latest
--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "${SCRIPT_DIR}" || exit
+
 echo "On first run do:
 terraform init
+gcloud auth login
 "
 
 echo "On every other run, run terraform commands as usual"
 
-docker-compose run tf bash
+if [ "$#" -eq 0 ]; then
+  my_args=(  bash )
+else
+  my_args=( $@ )
+fi
+
+docker-compose run tf $*
